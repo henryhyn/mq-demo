55dbf6d3bd8740fac8e70fc1ff5d64c059fd0c8c
何永能 <yongnenghe@xiaohongshu.com>
2018-09-15 09:13:53 +0800

    md

--- src/main/java/com/example/Job.java
+++ src/main/java/com/example/Job.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 基于消息队列的任务分配系统演示
+ * 演示方法:
+ * 1. 按下面第一篇参考文献安装 rabbitmq
+ * 2. 执行 UtProducerTest 全部测试
+ * 3. 执行 Job main 方法
  * 参考文献:
  * - [消息队列之 RabbitMQ](https://www.jianshu.com/p/79ca08116d57)
  * - [BlockingQueue（阻塞队列）详解](https://www.cnblogs.com/tjudzj/p/4454490.html)
