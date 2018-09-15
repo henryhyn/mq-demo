0a06d6b475ef94d10c68880929a64beeda4ffd8d
何永能 <yongnenghe@xiaohongshu.com>
2018-09-15 08:41:22 +0800

    md

--- src/main/java/com/example/Job.java
+++ src/main/java/com/example/Job.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by henry on 2018/8/8.
+ * 基于消息队列的任务分配系统演示
+ * 参考文献:
+ * - [消息队列之 RabbitMQ](https://www.jianshu.com/p/79ca08116d57)
+ * - [BlockingQueue（阻塞队列）详解](https://www.cnblogs.com/tjudzj/p/4454490.html)
  */
 @SpringBootApplication
 public class Job {
--- src/test/java/com/example/UtProducerTest.java
+++ src/test/java/com/example/UtProducerTest.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -16,21 +17,43 @@ import java.util.concurrent.TimeoutException;
 public class UtProducerTest {
     private static final String RABBITMQ_EXCHANGE_KEY = "hello-exchange";
 
-    @Test
-    public void testTaskProducer() throws IOException, TimeoutException, InterruptedException {
-        ConnectionFactory factory = new ConnectionFactory();
+    private ConnectionFactory factory;
+
+    @Before
+    public void before() {
+        factory = new ConnectionFactory();
         factory.setHost("localhost");
         factory.setUsername("guest");
         factory.setPassword("guest");
+    }
 
+    @Test
+    public void testTaskProducer() throws IOException, TimeoutException, InterruptedException {
         Connection conn = factory.newConnection();
         Channel channel = conn.createChannel();
         channel.exchangeDeclare(RABBITMQ_EXCHANGE_KEY, "direct", true);
 
         for (int i = 0; i < 10; i++) {
             channel.basicPublish(RABBITMQ_EXCHANGE_KEY, "task", null, String.format("task..%d", i).getBytes());
-            channel.basicPublish(RABBITMQ_EXCHANGE_KEY, "user", null, String.format("user..%d", i).getBytes());
             Thread.sleep(1000L);
         }
+
+        channel.close();
+        conn.close();
+    }
+
+    @Test
+    public void testUserProducer() throws IOException, TimeoutException, InterruptedException {
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+        channel.exchangeDeclare(RABBITMQ_EXCHANGE_KEY, "direct", true);
+
+        for (int i = 0; i < 10; i++) {
+            channel.basicPublish(RABBITMQ_EXCHANGE_KEY, "user", null, String.format("user..%d", i % 3).getBytes());
+            Thread.sleep(2000L);
+        }
+
+        channel.close();
+        conn.close();
     }
 }
